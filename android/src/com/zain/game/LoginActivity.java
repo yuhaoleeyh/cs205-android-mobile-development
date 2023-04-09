a256ecdb5e6361c04b2496a344b52c28adda715c
Rui Xian <tayrx9988@gmail.com>
2023-04-09 11:43:20 +0800

    feat

---
+++ android/src/com/zain/game/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.zain.game;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, AndroidLauncher.class);
+        startActivity(intent);
+    }
+}
