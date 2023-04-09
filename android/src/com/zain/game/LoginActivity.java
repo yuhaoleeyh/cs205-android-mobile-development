6f0c11f5f2add615a55f8fb5b0987150c8aeaf27
Rui Xian <tayrx9988@gmail.com>
2023-04-09 12:39:32 +0800

    feat: add username password input

--- android/src/com/zain/game/LoginActivity.java
+++ android/src/com/zain/game/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view){
         Intent intent = new Intent(this, AndroidLauncher.class);
+        EditText usernameInput = findViewById(R.id.usernameInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        intent.putExtra("username", username);
+
+        if (password != "1234") {
+            Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         startActivity(intent);
     }
 }
