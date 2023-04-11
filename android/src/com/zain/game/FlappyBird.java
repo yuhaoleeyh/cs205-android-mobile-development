b1ee01f5afae59b0663d7df02339997eaf27de6b
Khai Loong Aw <awkhailoong@gmail.com>
2023-04-11 11:27:44 +0800

    Move FlappyBird.java from core to android dir

--- android/src/com/zain/game/FlappyBird.java
+++ android/src/com/zain/game/FlappyBird.java
@@ -3,11 +3,9 @@ package com.zain.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
@@ -60,7 +58,7 @@ public class FlappyBird extends ApplicationAdapter {
 
     public FlappyBird(DatabaseHandler input_db) {
         db = input_db;
-    //    db.deleteAllRows();
+        db.deleteAllRows();
     }
 
     @Override
