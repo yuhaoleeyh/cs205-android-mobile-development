74dbb1b1d4d63cb9826fe1c4935a2fa8e50d8923
Lee Yu Hao <yuhaolee97@gmail.com>
2023-04-11 17:13:49 +0800

    Added the hiding of os status bar at the top

--- android/src/com/zain/game/LoginActivity.java
+++ android/src/com/zain/game/LoginActivity.java
@@ -6,16 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.view.WindowManager;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 
     public void login(View view){
+
         Intent intent = new Intent(this, AndroidLauncher.class);
         EditText usernameInput = findViewById(R.id.usernameInput);
         EditText passwordInput = findViewById(R.id.passwordInput);
@@ -23,10 +28,11 @@ public class LoginActivity extends AppCompatActivity {
         String password = passwordInput.getText().toString();
         intent.putExtra("username", username);
 
+
+
         if (!password.equals("hello")) {
             System.out.println(password);
             Toast.makeText(this, password, Toast.LENGTH_SHORT).show();
-            return;
         }
 
         startActivity(intent);
